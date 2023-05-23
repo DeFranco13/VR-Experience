23402c5ea64a8a9cd2b777087b8494cc1ce8a0bd
Saul-De-Clerck <s128108@student.ap.be>
2023-05-23 12:40:55 +0200

    add testtrack

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using System.Collections.Generic;
 
 public class CarAgent : Agent
 {
 	private Rigidbody rb;
+	List<GameObject> targets = new List<GameObject>();
 	int targetCount = 0;
 	int targetHitCount = 0;
 	public override void Initialize()
@@ -16,11 +18,15 @@ public class CarAgent : Agent
 
 	public override void OnEpisodeBegin()
 	{
-		if (this.transform.localPosition.y < 0)
+		foreach (var target in targets)
 		{
-			this.transform.localPosition = new Vector3(377.9497f, 1f, 635.1949f);
-			this.transform.localRotation = Quaternion.identity;
+			target.SetActive(true);
 		}
+		//if (this.transform.localPosition.y < 0)
+		//{
+			this.transform.position = new Vector3(315.2f, 5.64f, 719.7f);
+			this.transform.rotation= new Quaternion(0f,180f,0f,0f);
+		//}
 	}
 
 	public override void CollectObservations(VectorSensor sensor)
@@ -41,10 +47,15 @@ public class CarAgent : Agent
 
 		Vector3 movement = new Vector3(0f,0f, controlSignal.z * speedMultiplier * Time.deltaTime*1000);
 		Debug.Log(movement);
-		this.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * movement.z, ForceMode.Acceleration);
+		this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(transform.forward * movement.z, ForceMode.Acceleration);
 
 		//transform.localPosition += movement;
-
+		Debug.Log(transform.position.y);
+		if(transform.position.y < 5.0f)
+		{
+			AddReward(-0.2f);
+			EndEpisode();
+		}
 		if (movement.magnitude > 0.01f)
 		{
 			Vector3 rotate = new Vector3(0f, controlSignal.x * rotationSpeed * Time.deltaTime, 0f);
@@ -52,6 +63,11 @@ public class CarAgent : Agent
 		}
 
 		if (controlSignal.x != 0)
+		{
+			AddReward(-0.1f);
+		}
+
+		if (Mathf.Abs(controlSignal.x) > 20f)
 		{
 			AddReward(-0.1f);
 		}
@@ -66,20 +82,27 @@ public class CarAgent : Agent
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if (collision.gameObject.transform.parent.tag != "Track")
+		if (collision.gameObject.transform.parent.tag != "Track" && collision.gameObject.transform.parent.tag != "Target")
 		{
 			AddReward(-0.1f);
 			EndEpisode();
 		}
 		if (collision.gameObject.tag == "Target")
 		{
-			Destroy(collision.gameObject);
+			collision.gameObject.SetActive(false);
+			targets.Add(collision.gameObject);
 			AddReward(0.2f);
 			targetHitCount++;
 			Debug.Log("hit target");
 		}
+		if (collision.gameObject.tag == "Terrain")
+		{
+			AddReward(-0.2f);
+			EndEpisode();
+			Debug.Log("hit terrain");
+		}
 
-		if(targetHitCount >= targetCount)
+		if (targetHitCount >= targetCount)
 		{
 			AddReward(1f);
 			EndEpisode();

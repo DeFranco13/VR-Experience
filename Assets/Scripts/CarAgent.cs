aeb30d3f1ccc96e3e23968ae86ae8dec00a30c96
tibo2002 <tibo.vandersanden@telenet.be>
2023-06-09 01:23:52 +0200

    changes

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -11,26 +11,38 @@ namespace UnityStandardAssets.Vehicles.Car
 	public class CarAgent : Agent
 	{
 		private CarController carController;
-		int toHit = 1;
+		int toHit = 0;
 		private Rigidbody rb;
 		List<GameObject> targets = new List<GameObject>();
 		int targetCount = 0;
 		int targetHitCount = 0;
-		public override void Initialize()
+
+		// collide bools
+		Boolean collidedWithTarget = false;
+        Boolean collidedWithTerainOrElse = false;
+
+
+        public override void Initialize()
 		{
 			rb = GetComponent<Rigidbody>();
 			targetCount = GameObject.FindGameObjectsWithTag("Target").Length;
+			Debug.Log($"targets to hit {targetCount}");
 			carController = GetComponent<CarController>();
 		}
 
 		public override void OnEpisodeBegin()
 		{
-			toHit = 1;
-			foreach (var target in targets)
-			{
-				target.SetActive(true);
-			}
-			this.transform.position = new Vector3(382.6f, 1.1f, 625.82f);
+			toHit = 0;
+			targetHitCount = 0;
+			toHit = 0;
+			//carController.Move(0, 0, 0, 0);
+			//set the targets active again every new episode
+            foreach (var target in targets)
+            {
+                target.SetActive(true);
+            }
+
+            this.transform.position = new Vector3(382.6f, 1.1f, 625.82f);
 			this.transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
 		}
 
@@ -38,8 +50,8 @@ namespace UnityStandardAssets.Vehicles.Car
 		{
 			// Target en Agent posities
 			sensor.AddObservation(this.transform.localPosition);
-			sensor.AddObservation(this.transform.localRotation);
-		}
+            sensor.AddObservation(this.transform.localRotation);
+        }
 
 		public float speedMultiplier = 0.5f;
 		public float rotationSpeed = 100f;
@@ -47,36 +59,54 @@ namespace UnityStandardAssets.Vehicles.Car
 		public override void OnActionReceived(ActionBuffers actionBuffers)
 		{
 			Vector3 controlSignal = Vector3.zero;
-			controlSignal.z = -actionBuffers.ContinuousActions[0];
-			controlSignal.x = actionBuffers.ContinuousActions[1];
-			controlSignal.y = actionBuffers.ContinuousActions[2];
-			carController.Move(controlSignal.z, controlSignal.x, controlSignal.x, controlSignal.y);
-			//Vector3 movement = new Vector3(0f,0f, controlSignal.z * speedMultiplier * Time.deltaTime*1000);
-			//Debug.Log(movement);
-			//this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(transform.forward * movement.z, ForceMode.Acceleration);
 
-			//transform.localPosition += movement;
-			if (transform.position.y < 5.0f)
+            controlSignal.z = actionBuffers.ContinuousActions[0];
+            controlSignal.x = actionBuffers.ContinuousActions[1];
+            controlSignal.y = actionBuffers.ContinuousActions[2];
+
+			
+            //beweeg de auto
+            carController.Move(controlSignal.x, controlSignal.z, controlSignal.z, controlSignal.y);
+			
+            /*
+			 * 1) moest de auto voor een of andere reden naar boven bewegen stoppen we de episode
+			 * 2) we geven een positieve reward wanneer de auto een target hit en als hij een ronde heeft uitgereden
+			 * 3) we geven een negatieve reward wanneer hij de zijmuren raakt 
+			 * of er door zou rijden en van de track zo gaan
+			*/
+            if (transform.position.y > 10.0f)
 			{
 				AddReward(-0.2f);
 				EndEpisode();
-			}
-			//if (movement.magnitude > 0.01f)
-			//{
-			//	Vector3 rotate = new Vector3(0f, controlSignal.x * rotationSpeed * Time.deltaTime, 0f);
-			//	transform.Rotate(rotate);
-			//}
-
-			if (controlSignal.x != 0)
-			{
-				AddReward(-0.1f);
+				Debug.Log("too high");
 			}
 
-			if (Mathf.Abs(controlSignal.x) > 20f)
-			{
-				AddReward(-0.1f);
-			}
-		}
+            if (collidedWithTarget)
+            {
+                targetHitCount++;
+                toHit++;
+
+                float targetReward = (float)targetHitCount / 66f; // Gradually increasing reward up to 1.0
+
+                SetReward(targetReward);
+
+                if (targetHitCount >= targetCount)
+                {
+                    SetReward(1f);
+                    EndEpisode();
+                }
+
+				collidedWithTarget = false;
+            }
+
+            if (collidedWithTerainOrElse)
+            {
+                AddReward(-0.2f);
+                EndEpisode();
+				collidedWithTerainOrElse = false;
+                Debug.Log("hit terrain");
+            }
+        }
 
 		public override void Heuristic(in ActionBuffers actionsOut)
 		{
@@ -84,68 +114,52 @@ namespace UnityStandardAssets.Vehicles.Car
 			continuousActionsOut[0] = Input.GetAxis("Horizontal");
 			continuousActionsOut[1] = Input.GetAxis("Vertical");
 			continuousActionsOut[2] = Input.GetAxis("Jump");
-			//Debug.Log(continuousActionsOut);
-			//continuousActionsOut[0] = Input.GetAxis("Vertical");
-			//continuousActionsOut[1] = Input.GetAxis("Horizontal");
+
 		}
 
 		private void OnTriggerEnter(Collider other)
 		{
+
 			string name = "";
+			int targetNumber = 0;
 			try
 			{
 				name = other.transform.parent.name.Split('(')[1];
 				name = name.Substring(0, name.Length - 1);
+				targetNumber = int.Parse(name);
 			}
 			catch (Exception)
 			{
 			}
 
-			if (other.tag.Trim() == "Target" && name == toHit.ToString())
+			//Debug.Log("target " + toHit.ToString());
+            //Debug.Log(targetNumber);
+
+            if (targetNumber == toHit)
 			{
-				other.gameObject.SetActive(false);
-				targets.Add(other.gameObject);
-				AddReward(0.2f);
-				targetHitCount++;
-				Debug.Log("add hit");
-				toHit++;
-				Debug.Log("hit target");
+                Debug.Log("hit target");
+
+                targets.Add(other.gameObject);
+                collidedWithTarget = true;
+                other.gameObject.SetActive(false);
 			}
-			else
-			{
-				if (name == "target" || name == "")
-				{
-					AddReward(0.2f);
-				}
-				else
-				{
-					Debug.Log("wrong target");
-					AddReward(-0.1f);
-					EndEpisode();
-				}
-			}
-		}
+        }
 
 
 		private void OnCollisionEnter(Collision collision)
 		{
-			if (collision.gameObject.transform.parent.tag != "Track")
+
+
+            if (collision.gameObject.transform.parent.tag != "Track")
 			{
-				AddReward(-0.1f);
-				EndEpisode();
+				collidedWithTerainOrElse = true;
+
 			}
 
 			if (collision.gameObject.tag == "Terrain")
 			{
-				AddReward(-0.2f);
-				EndEpisode();
-				Debug.Log("hit terrain");
-			}
-
-			if (targetHitCount >= targetCount)
-			{
-				AddReward(1f);
-				EndEpisode();
+                collidedWithTerainOrElse = true;
+                
 			}
 		}
 

5e95c4203a32f5e3d395183075f5960162a107c6
tibo2002 <tibo.vandersanden@telenet.be>
2023-06-14 11:29:25 +0200

    push

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -18,8 +18,8 @@ namespace UnityStandardAssets.Vehicles.Car
 		int targetHitCount = 0;
 
 		// collide bools
-		Boolean collidedWithTarget = false;
-        Boolean collidedWithTerainOrElse = false;
+		bool collidedWithTarget = false;
+        bool collidedWithTerainOrElse = false;
 
 
         public override void Initialize()
@@ -35,7 +35,7 @@ namespace UnityStandardAssets.Vehicles.Car
 			toHit = 0;
 			targetHitCount = 0;
 			toHit = 0;
-			//carController.Move(0, 0, 0, 0);
+			carController.Move(0, 0, 0, 0);
 			//set the targets active again every new episode
             foreach (var target in targets)
             {
@@ -53,60 +53,8 @@ namespace UnityStandardAssets.Vehicles.Car
             sensor.AddObservation(this.transform.localRotation);
         }
 
-		public float speedMultiplier = 0.5f;
-		public float rotationSpeed = 100f;
-
-		public override void OnActionReceived(ActionBuffers actionBuffers)
-		{
-			Vector3 controlSignal = Vector3.zero;
-
-            controlSignal.z = actionBuffers.ContinuousActions[0];
-            controlSignal.x = actionBuffers.ContinuousActions[1];
-            controlSignal.y = actionBuffers.ContinuousActions[2];
-
-			
-            //beweeg de auto
-            carController.Move(controlSignal.x, controlSignal.z, controlSignal.z, controlSignal.y);
-			
-            /*
-			 * 1) moest de auto voor een of andere reden naar boven bewegen stoppen we de episode
-			 * 2) we geven een positieve reward wanneer de auto een target hit en als hij een ronde heeft uitgereden
-			 * 3) we geven een negatieve reward wanneer hij de zijmuren raakt 
-			 * of er door zou rijden en van de track zo gaan
-			*/
-            if (transform.position.y > 10.0f)
-			{
-				AddReward(-0.2f);
-				EndEpisode();
-				Debug.Log("too high");
-			}
-
-            if (collidedWithTarget)
-            {
-                targetHitCount++;
-                toHit++;
-
-                float targetReward = (float)targetHitCount / 66f; // Gradually increasing reward up to 1.0
-
-                SetReward(targetReward);
-
-                if (targetHitCount >= targetCount)
-                {
-                    SetReward(1f);
-                    EndEpisode();
-                }
-
-				collidedWithTarget = false;
-            }
-
-            if (collidedWithTerainOrElse)
-            {
-                AddReward(-0.2f);
-                EndEpisode();
-				collidedWithTerainOrElse = false;
-                Debug.Log("hit terrain");
-            }
-        }
+		private float speedMultiplier = 10f;
+		private float rotationSpeed = 10f;
 
 		public override void Heuristic(in ActionBuffers actionsOut)
 		{
@@ -116,6 +64,53 @@ namespace UnityStandardAssets.Vehicles.Car
 			continuousActionsOut[2] = Input.GetAxis("Jump");
 
 		}
+		public override void OnActionReceived(ActionBuffers actionBuffers)
+		{
+			Vector3 controlSignal = Vector3.zero;
+
+			controlSignal.x = actionBuffers.ContinuousActions[0];
+            controlSignal.z = actionBuffers.ContinuousActions[1];
+            controlSignal.y = actionBuffers.ContinuousActions[2];
+
+
+			controlSignal.x = controlSignal.x * 10;
+			controlSignal.z = controlSignal.z * 10;
+
+
+            //beweeg de auto
+            carController.Move(controlSignal.x, controlSignal.z, 0f,controlSignal.y);
+			
+            /*
+			 * 1) moest de auto voor een of andere reden naar boven bewegen stoppen we de episode
+			 * 2) we geven een positieve reward wanneer de auto een target hit en als hij een ronde heeft uitgereden
+			 * 3) we geven een negatieve reward wanneer hij de zijmuren raakt 
+			 * of er door zou rijden en van de track zo gaan
+			*/
+
+
+            if (transform.position.y > 10.0f)
+			{
+				AddReward(-0.2f);
+				EndEpisode();
+				Debug.Log("too high");
+			}
+
+            if (collidedWithTarget)
+            {
+				targetHitCount++;
+				toHit++;
+
+				AddReward(0.5f);
+
+                if (targetHitCount >= targetCount)
+                {
+                    AddReward(5f);
+                    EndEpisode();
+                }
+
+				collidedWithTarget = false;
+            }
+        }
 
 		private void OnTriggerEnter(Collider other)
 		{
@@ -148,20 +143,46 @@ namespace UnityStandardAssets.Vehicles.Car
 
 		private void OnCollisionEnter(Collision collision)
 		{
-
-
-            if (collision.gameObject.transform.parent.tag != "Track")
+            if (collision.gameObject.transform.tag == "TrackObject")
 			{
-				collidedWithTerainOrElse = true;
-
-			}
-
-			if (collision.gameObject.tag == "Terrain")
-			{
-                collidedWithTerainOrElse = true;
-                
+				Debug.Log("hit wall");
+                AddReward(-0.5f);
 			}
 		}
+
+		private float wallCollisionTimer = 0.0f;
+		private float wallCollisionThreshold = 10.0f;
+		private bool isWallCollisionTimerRunning = false;
+
+		private void OnCollisionStay(Collision collision)
+		{
+			if (collision.gameObject.transform.tag == "TrackObject")
+			{
+				Debug.Log("keeps hitting wall");
+				AddReward(-0.05f);
+
+				// Start or update the wall collision timer
+				if (!isWallCollisionTimerRunning)
+				{
+					wallCollisionTimer = 0.0f;
+					isWallCollisionTimerRunning = true;
+				}
+				else
+				{
+					wallCollisionTimer += Time.fixedDeltaTime;
+				}
+
+				// Check if the wall collision timer has reached the threshold
+				if (wallCollisionTimer >= wallCollisionThreshold)
+				{
+					AddReward(-0.05f);
+					wallCollisionTimer = 0.0f;
+					Debug.Log("episode ended");
+					EndEpisode();
+				}
+			}
+		}
+
 
 	}
 }

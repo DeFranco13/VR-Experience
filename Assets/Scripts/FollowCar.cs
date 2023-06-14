fcac117c3a5aa75e29e546faebf2fe5e0d93077f
Victor De Decker <s129144@ap.be>
2023-06-14 16:38:36 +0200

    add menu screen

--- Assets/Scripts/FollowCar.cs
+++ Assets/Scripts/FollowCar.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class FollowCar : MonoBehaviour
 {
-    [SerializeField] GameObject car;
-    [SerializeField] Vector3 startPos;
-
-    private void Start()
-    {
-        if(startPos==null)
-            startPos = new Vector3(0.01f, 0.56f, -0.4f);
-    }
+    [SerializeField] GameObject buttonPlaceholder;
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = car.transform.position; //+ startPos;
-        this.transform.rotation = Quaternion.Euler(new Vector3(-car.transform.rotation.x, -car.transform.rotation.y -90, car.transform.rotation.z - 69.838f));
+        transform.position = buttonPlaceholder.transform.position;
+        transform.localRotation = transform.rotation = Quaternion.Euler(buttonPlaceholder.transform.eulerAngles.x, buttonPlaceholder.transform.eulerAngles.y + 90f, buttonPlaceholder.transform.eulerAngles.z - 69.838f);
     }
 }

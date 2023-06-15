2110a84ac99b6bdae01e0ba100598a2e8c435e0a
NoahPladys <pladys.noah@gmail.com>
2023-06-15 14:15:12 +0200

    Add spectator scene

---
+++ Assets/Scripts/XRCameraSwitcher.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XRCameraSwitcher : MonoBehaviour
+{
+    public GameObject[] CameraRigs;
+    private int currentCameraIndex = 0;
+
+    private void Start()
+    {
+        foreach(GameObject rig in CameraRigs)
+        {
+            rig.SetActive(false);
+        }
+        activateCameraRig(currentCameraIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            nextCameraRig();
+        }
+    }
+
+    private void nextCameraRig()
+    {
+        disabledCameraRig(currentCameraIndex);
+        currentCameraIndex = (currentCameraIndex + 1) % CameraRigs.Length;
+        activateCameraRig(currentCameraIndex);
+    }
+
+    private void activateCameraRig(int cameraRigIndex)
+    {
+        CameraRigs[cameraRigIndex].gameObject.SetActive(true);
+    }
+
+    private void disabledCameraRig(int cameraRigIndex)
+    {
+        CameraRigs[cameraRigIndex].gameObject.SetActive(false);
+    }
+}

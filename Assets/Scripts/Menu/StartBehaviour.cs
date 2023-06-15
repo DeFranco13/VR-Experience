d7f1228009280c7d79a371545f20611dc331b738
V1ctorrrr <91462950+V1ctorrrr@users.noreply.github.com>
2023-06-15 18:04:25 +0200

    Noah (#63)
    * Add racestattracker
    
    * Add spectator menu option
    
    * Add spectator scene
    
    * Spectators track 1 and spectator script
    
    * RaceStatTracker script changed
    
    * prepare merge
    
    * add menu for spectators
    
    ---------
    
    Co-authored-by: NoahPladys <pladys.noah@gmail.com>
    Co-authored-by: Victor De Decker <s129144@ap.be>

--- Assets/Scripts/Menu/StartBehaviour.cs
+++ Assets/Scripts/Menu/StartBehaviour.cs
@@ -2,16 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartBehaviour : MonoBehaviour
 {
     public TrackSelector trackSelector;
     public CarSelector carSelector;
-
-    void Update()
-    {
-        //Debug.Log(trackSelector.GetSelectedTrackName() + " | " + carSelector.GetSelectedCarName());
-    }
+    public Toggle spectateToggle;
 
     public void Play()
     {
@@ -20,6 +17,8 @@ public class StartBehaviour : MonoBehaviour
 
         //Track
         string sceneName = trackSelector.GetSelectedTrackName();
+        if (spectateToggle.isOn)
+            sceneName += " - Spectator";
         SceneManager.LoadScene(sceneName);
     }
 }

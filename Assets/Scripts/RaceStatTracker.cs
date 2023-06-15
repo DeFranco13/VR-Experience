2eb2c4064c7791f96d01b9a89f24703c9ad5645d
NoahPladys <pladys.noah@gmail.com>
2023-06-15 20:29:37 +0200

    add countdown

--- Assets/Scripts/RaceStatTracker.cs
+++ Assets/Scripts/RaceStatTracker.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Vehicles.Car;
 
 public class RaceStatTracker : MonoBehaviour
 {
     public GameObject FinishLineCollider;
     public int NumberOfLaps;
+    public MonoBehaviour UserCarController;
+    public List<MonoBehaviour> AICarAgents;
 
+    public List<GameObject> lights;
+    public Material RedLightMaterial;
+    public Material GreenLightMaterial;
+
+    private bool raceStarted = false;
     private int currentLap;
+    private float countdown = 0;
+    private float raceTime = 0;
+    private float raceEndedCountdown = 0;
     public int CurrentLap
     {
         get
@@ -18,6 +30,58 @@ public class RaceStatTracker : MonoBehaviour
             if (currentLap >= NumberOfLaps) return NumberOfLaps;
             return currentLap; 
         }
+    }
+
+    void Start()
+    {
+        UserCarController.enabled = false;
+        AICarAgents.ForEach(
+            (agent) => 
+            {
+                agent.enabled = false;
+            }
+        );
+    }
+
+    void Update()
+    {
+        if(!raceStarted)
+        {
+            countdown += Time.deltaTime;
+
+            if (countdown >= 4)
+                lights[0].GetComponent<MeshRenderer>().material = RedLightMaterial;
+            if (countdown >= 5.5)
+                lights[1].GetComponent<MeshRenderer>().material = RedLightMaterial;
+            if (countdown >= 7)
+                lights[2].GetComponent<MeshRenderer>().material = RedLightMaterial;
+            if (countdown >= 8.5)
+            {
+                lights.ForEach(
+                    (light) =>
+                    {
+                        light.GetComponent<MeshRenderer>().material = GreenLightMaterial;
+                    }
+                );
+                StartRace();
+                raceStarted = true;
+            }
+        } 
+        else
+        {
+            raceTime += Time.deltaTime;
+        }
+    }
+
+    void StartRace()
+    {
+        UserCarController.enabled = true;
+        AICarAgents.ForEach(
+            (agent) =>
+            {
+                agent.enabled = true;
+            }
+        );
     }
 
     private void OnTriggerEnter(Collider other)
